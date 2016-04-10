d113b823301d9862b236337be01df191d3f61242
Tim Dussinger <tritowntim@gmail.com>
2016-04-09 20:02:22 -0400

    CSS modules

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-var config = require("../config.json");
+import config from '../config.json';
+import styles from './Greeter.css';
 
 class Greeter extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.root}>
         {config.greetText}
       </div>
     );
